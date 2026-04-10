6774822df96c71e691e550ddda6345d30c9ef7a1
JFabian2606 <jose.fabian.ortiz@correounivalle.edu.co>
2026-04-10 18:22:02 -0500

    feat: SCRUM-16 Implementar funcion eliminar producto

---
+++ android/app/src/main/kotlin/com/example/pocketpos/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pocketpos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/db/db_helper.dart
@@ -0,0 +1,64 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:pocketpos/models/models.dart';
+
+class DBHelper {
+  static Database? _db;
+
+  static Future<Database> get database async {
+    if (_db != null) return _db!;
+    _db = await _initDB();
+    return _db!;
+  }
+
+  static Future<Database> _initDB() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, 'pocketpos.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE products(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            price REAL NOT NULL,
+            stock INTEGER NOT NULL,
+            category TEXT NOT NULL
+          )
+        ''');
+      },
+    );
+  }
+
+  // Insertar producto
+  static Future<int> insertProduct(Product p) async {
+    final db = await database;
+    return await db.insert('products', p.toJson()..remove('id'));
+  }
+
+  // Obtener todos los productos
+  static Future<List<Product>> getProducts() async {
+    final db = await database;
+    final result = await db.query('products');
+    return result.map((e) => Product.fromJson(e)).toList();
+  }
+
+  // Actualizar producto
+  static Future<int> updateProduct(Product p) async {
+    final db = await database;
+    return await db.update(
+      'products',
+      p.toJson(),
+      where: 'id = ?',
+      whereArgs: [p.id],
+    );
+  }
+
+  // Eliminar producto
+  static Future<int> deleteProduct(int id) async {
+    final db = await database;
+    return await db.delete('products', where: 'id = ?', whereArgs: [id]);
+  }
+}
